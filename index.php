21c3bc37a403e3afdf441327606a95b0811a77cc
Amper-r <repidav@gmail.com>
2022-04-22 19:36:36 +0300

    Add error handler

--- index.php
+++ index.php
@@ -5,14 +5,44 @@
 
     $connect = ConnectDb();
 
-    $query = $connect->query("SELECT * FROM `products`");
-
-    while($row = $query->fetch_assoc()){
-        $data[] = $row;
+    $fun = $_GET["task"];
+    try {
+        $fun($connect, $_GET["uslugaId"]);
+    } catch (\Throwable $th) {
+        die("<span style=font-size:16px;><b>Error:</b> Function ".$fun." not found</span>");
     }
 
 
-    $status = 200;
-    header('HTTP/1.1 '.$status.' Unauthorized', true, $status);
-    echo json_encode($data);
+    function getUslugi($connect, $id){
+        $query = $connect->query("SELECT * FROM products WHERE has_electonic_view=1");
+        while($row = $query->fetch_assoc()){
+            $data[] = $row;
+        }
+
+        if(!$data){
+            die("<span style=font-size:16px;><b>Error:</b> Not found</span>");
+        }
+
+        $status = 200;
+        header('HTTP/1.1 '.$status.' Unauthorized', true, $status);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+
+    function getUsluga($connect, $id){
+        if($id > 0){
+            $query = $connect->query("SELECT * FROM products WHERE id=".$id."");
+            $data = $query->fetch_assoc();
+            
+            if(!$data){
+                die("<span style=font-size:16px;><b>Error:</b> Record with <b>ID=".$id."</b> not found</span>");
+            }
+
+            $status = 200;
+            header('HTTP/1.1 '.$status.' Unauthorized', true, $status);
+            echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        }
+        else{
+            die("<span style=font-size:16px;><b>Error:</b> The ID parameter is undefined or less than or equal to 0</span>");
+        }
+    }
 ?>
--- phpFiles/databaseConnect.php
+++ phpFiles/databaseConnect.php
@@ -9,11 +9,11 @@
         $connect = new mysqli($server, $username, $password, $dbname);
 
         if($connect->connect_error){
-            die("Ошибка соединения");
+            die("<span style=font-size:16px;><b>Error:</b> Database connection</span>");
         }
 
         if(!$connect->set_charset($charset)){
-            echo "Ошибка установки кодировки";
+            echo "<span style=font-size:16px;><b>Error:</b> setting encoding</span>";
         }
 
         return $connect;
