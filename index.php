6b2c1ab14deae2dd0a8576ba3d6addca5a1cc79a
Amper-r <repidav@gmail.com>
2022-04-24 13:24:00 +0300

    add ftp upload xml

--- index.php
+++ index.php
@@ -4,39 +4,56 @@
     require_once __DIR__ ."./phpFiles/xml.php";
     //RecordJson();
 
+    //FTP XML UPLOAD
+    $ftp_server = '37.140.192.158';
+    $ftp_user_name = 'u1656895_practic';
+    $ftp_user_pass = 'iJ2rC8cN9x';
+    $ftp_data = array("ftp_server" => $ftp_server, "ftp_user_name" => $ftp_user_name, "ftp_user_pass" => $ftp_user_pass);
+
     $connect = ConnectDb();
 
     $fun = $_GET["task"];
     $ids = $_GET["uslugaId"];
-        if(isset($fun)){
-            $fun($connect, $ids);
-        }
+    $has_electonic_view = $_GET["has_electonic_view"];
     try {
-        // $ids = $_GET["uslugaId"];
-        // if(isset($fun)){
-        //     $fun($connect, $ids);
-        // }
+        if(isset($fun)){
+            $fun($connect, $ids, $has_electonic_view, $ftp_data);
+        }
     } catch (\Throwable $th) {
-        //die("<span style=font-size:16px;><b>Error:</b> Function ".$fun." not found</span>");
+        echo $th."<br>";
+        die("<span style=font-size:16px;><b>Error:</b> Function ".$fun." not found</span>");
     }
 
 
-    function getUslugi($connect, $id){
-        $query = $connect->query("SELECT * FROM products WHERE has_electonic_view=1");
+    function getUslugi($connect, $id, $has_electonic_view, $ftp_data){
+        if(!isset($has_electonic_view)){
+            $has_electonic_view = 1;
+        }
+        $has_electonic_view_str =  $has_electonic_view < 0 ? "" : "WHERE has_electonic_view=".$has_electonic_view;
+
+        $query = $connect->query("SELECT * FROM products ".$has_electonic_view_str."");
         while($row = $query->fetch_assoc()){
             $data[] = $row;
         }
 
         if(!$data){
-            die("<span style=font-size:16px;><b>Error:</b> Not found</span>");
+            die("<span style=font-size:16px;><b>Error:</b> No data found with the specified parameters</span>");
         }
 
+        UpdateXML($data, "./result.xml", $ftp_data["ftp_server"], $ftp_data["ftp_user_name"], $ftp_data["ftp_user_pass"]);
         $status = 200;
         header('HTTP/1.1 '.$status.' Unauthorized', true, $status);
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
     }
 
-    function getUsluga($connect, $ids){
+    function getUsluga($connect, $ids, $has_electonic_view, $ftp_data){
+        if(!isset($ids)){
+            die("<span style=font-size:16px;><b>Error:</b> Missing uslugaId argument</span>");
+        }
+        if(!is_array($ids)){
+            $ids = array($ids);
+        }
+
         $data = array();
         foreach ($ids as $id) {
             if($id > 0){
@@ -54,7 +71,7 @@
             }
         }
         if($data){
-            UpdateXML($data, "./result.xml");
+            UpdateXML($data, "./result.xml", $ftp_data["ftp_server"], $ftp_data["ftp_user_name"], $ftp_data["ftp_user_pass"]);
             $status = 200;
             header('HTTP/1.1 '.$status.' Unauthorized', true, $status);
             echo json_encode($data, JSON_UNESCAPED_UNICODE);
